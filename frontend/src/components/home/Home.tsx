7a10430224a430975256adc3199db38696790dcf
Andrew <olesik.andrei@gmail.com>
2023-01-21 13:58:07 +0100

    home fix

--- frontend/src/components/home/Home.tsx
+++ frontend/src/components/home/Home.tsx
@@ -9,46 +9,46 @@ const { Panel } = Collapse;
 const ClientsView = () => {
     const handleDelete = (key: React.Key) => {
 
-};
+    };
 
     const [data, setData] = useState([]);
-  useEffect(() => {
+    useEffect(() => {
         getClientsData(setData)
     }, [])
     const columns = [
-    {
-      title: 'Pesel',
+        {
+            title: 'Pesel',
             key: 'pesel',
             render: (text: any, record: any) => <>{record.pesel}</>,
-    },
-    {
-      title: 'Imie',
+        },
+        {
+            title: 'Imie',
             key: 'imie',
             render: (text: any, record: any) => <>{record.imie}</>,
-    },
-    {
-      title: 'Nazwisko',
+        },
+        {
+            title: 'Nazwisko',
             key: 'nazwisko',
-            render: (text:any, record :any)=> <>{record.nazwisko}</>,
-    },
-    {
+            render: (text: any, record: any) => <>{record.nazwisko}</>,
+        },
+        {
             title: 'Numer telefonu',
             key: 'telefon',
             render: (text: any, record: any) => <>{record.numer_telefonu}</>,
-    },
+        },
         {
             title: 'Addres',
             render: (text: any, record: any) => <a href={"https://www.google.com/maps/search/?api=1&query=" + record.adres.replace(' ', '+')}>{record.adres}</a>,
             key: 'adres',
         },
-        
-    {
-      title: 'Numer telefonu',
+
+        {
+            title: 'Numer telefonu',
             key: 'data_urodzenia',
             render: (text: any, record: any) => <>{record.data_urodzenia}</>,
         },
-      
-    {
+
+        {
             title: 'Podróże',
             key: 'podroze',
             render: (text: any, record: any) =>
@@ -68,25 +68,25 @@ const ClientsView = () => {
                     </Collapse >
                     :
                     <>Brak danych</>
-      
+
                 }</>
-    },
-    {
+        },
+        {
             title: 'Akcja',
             render: (text: any, record: any) => <>
                 <a href={"/klienty/edycja/" + record.pesel}>Edytuj</a><br />
                 <a href={"/klienty/dodaj/"}>dodaj</a><br />
                 <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record.pesel)}>
                     <a>Usuń</a>
-          </Popconfirm>
+                </Popconfirm>
             </>
-    },
+        },
     ]
-  return (
-    <div>
+    return (
+        <div>
             <h2>Clienty</h2>
             <Table columns={columns} dataSource={data} />
-    </div>
+        </div>
     )
 }
 export default ClientsView
